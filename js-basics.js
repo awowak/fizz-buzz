08b4062a55615070cdd54ac16f191bc0d3fcb986
Amy Wowak <akwowak@gmail.com>
2016-06-20 20:57:32 -0400

    1-2 completed

---
+++ js-basics.js
@@ -0,0 +1,68 @@
+// 1. Create a "cheat sheet" JavaScript file that contains working examples with comments of:
+// Variables: container for a value
+// Debugging (alerts, comments, the console): debug in chrome, console.log, breakpoints
+// Different data types: string, Boolean, number, undetermined
+// Arrays: holds collection of data
+// Testing: test in console log, repl.it
+// Logic: if, else
+// Functions: arguments, returns value
+
+// 2. Write 3 different functions that take input and return something via the console or an alert.
+
+for(var i=10; i>0; i--) {
+    console.log(i);
+}
+console.log("go!!!");
+
+for(var i=0;i<=20 ; i++) {
+    if(i % 2 === 0) {
+        console.log(i + " is even");
+    } else {
+        console.log(i + " is odd");
+    }
+}
+
+var rainbowColors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet'];
+for (var i = 0; i < rainbowColors.length; i++) {
+  console.log(rainbowColors[i]);
+}
+
+function halfOf(y) {
+    var halvsies = y / 2;
+    console.log('Half of ' + y + ' is ' +  halvsies);
+    return halvsies;
+}
+
+halfOf(5);
+
+// **Complete four of the following problems**
+
+// 3. Create a script with two variables assigned to two numbers. Add them together and output the result to the console. Now do the same with two strings.
+
+// 4. Create a multidimensional array related to a subject that interests you. Output two of the items in sub-arrays to the console.
+
+// 5. Write a script that checks if a variable is less than 10. If it is, alert the user that their variable is less than 10. If it is not, using the console, let the user know that what the variable was and that it was greater than 10.
+
+// 6. Try running the script and then changing the variable's value to see how this affects the program's output.
+
+// 7. Write a similar program to check if a string stored in a variable is the same as another string.
+
+// 8. Declare a function that takes a name as an argument and tells the user what name they've entered. Try running it after it has been declared.
+
+// 9. Declare a function that takes no arguments but prints something to the console. Try running it after it has been declared.
+
+// ### Additional Problem
+
+// 10. Declare a function that depending upon which virtual 'door' was entered tells the user they've received a different 'prize' in an alert. After declaring the function, try running it with different options.
+
+// ## Grading Criteria
+
+// 1. The "cheat sheet" is for the student's own use, so while it is required to be submitted, it doesn't need to be completely syntactically correct.
+
+// 2. The syntax is correct and the code is indented properly.
+
+// 3. All scripts should output the expected result.
+
+// 4. At least 4 of problems 3 through 9 need to be completed.
+
+
